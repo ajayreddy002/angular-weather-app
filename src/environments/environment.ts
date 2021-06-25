60ebc60dcd3c374a6876d4fdee37ffcdcd7ec75a
Ajaykumar-at-619991248891 <ajaykumar@skilltechnologies.in>
2021-06-25 20:20:23 +0530

    completed task

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'825038e0287bed2ecc475c9b51630893',
+  apiURL: 'https://api.openweathermap.org/data/2.5/weather'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
